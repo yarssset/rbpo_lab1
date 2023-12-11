c9ca937055bb9c40161c787bc82adcf23da4f914
yarssset <76969319+yarssset@users.noreply.github.com>
2023-12-11 20:29:29 +0300

    WritePersonData.cpp

--- lab_1/task_2/WritePersonData.cpp
+++ lab_1/task_2/WritePersonData.cpp
@@ -3,5 +3,6 @@
 string salary_string;
 
 void WritePersonData(const string& name, const string& height, const string& weight, const unsigned short* age){ // Осуществляет вывод на экран имени, возраста, роста, веса, заработной платы человека
+    setlocale(LC_ALL, "Russian");
     cout << "Имя: " << name << ". Рост: " << height << ". Вес: " << weight << ". Возраст: " << *age << ". Заработная плата: " << salary_string << endl;
 }
