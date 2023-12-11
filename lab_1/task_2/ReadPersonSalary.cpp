258349f07291280848634effb667f6cff809973c
yarssset <76969319+yarssset@users.noreply.github.com>
2023-12-11 20:21:49 +0300

    Delete lab_1 directory

--- lab_1/task_2/ReadPersonSalary.cpp
+++
@@ -1,5 +0,0 @@
-#include "Header.h"
-void ReadPersonSalary(double* salary) { // Осуществляет ввод с клавиатуры заработной платы человека
-    cout << "Введите заработную плату" << endl;
-    cin >> *salary;
-}
--- lab_1/task_2/WritePersonData.cpp
+++
@@ -1,7 +0,0 @@
-#include "Header.h"
-
-string salary_string;
-
-void WritePersonData(const string& name, const string& height, const string& weight, const unsigned short* age) { // Осуществляет вывод на экран имени, возраста, роста, веса, заработной платы человека
-    cout << "Имя: " << name << ". Рост: " << height << ". Вес: " << weight << ". Возраст: " << *age << ". Заработная плата: " << salary_string << endl;
-}
